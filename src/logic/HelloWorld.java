2b11379f7aea021b02f9b07474d1265d2e628e18
alessandrochillotti <alessandro.chillotti@outlook.it>
2021-03-14 15:02:04 +0100

    Fixed string to print

--- src/logic/HelloWorld.java
+++ src/logic/HelloWorld.java
@@ -4,7 +4,7 @@ import java.util.logging.Logger;
 
 public class HelloWorld {
 
-	private static final Logger LOGGER = Logger.getLogger("Hello, World!");
+	private static final Logger LOGGER = Logger.getLogger("Hello World!");
 	
 	public static void main(String[] args) {
 		LOGGER.info(LOGGER.getName());
